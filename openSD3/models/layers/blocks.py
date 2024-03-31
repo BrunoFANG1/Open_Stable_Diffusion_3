e104fdba666e63285328d884a558dfc6d8dd3f52
BrunoFANG1 <1324441676@qq.com>
2024-03-31 21:19:03 +0000

    Update time_embedding from openai

--- openSD3/models/layers/blocks.py
+++ openSD3/models/layers/blocks.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import einops
+import math
 
 def modulate(norm_func, x, scale, shift):
     # Suppose x is (B, N, D), shift is (B, D), scale is (B, D)
@@ -23,6 +24,49 @@ class RMSNorm(nn.Module):
         RMS = x.norm(2, dim=-1, keepdim = True)* (self.dim_len)**(-0.5)
         x = x/(RMS + self.epsilon) * self.g
         return x
+
+
+class TimestepEmbedder(nn.Module):
+    """
+    Embeds scalar timesteps into vector representations.
+    """
+
+    def __init__(self, hidden_size, frequency_embedding_size=256):
+        super().__init__()
+        self.mlp = nn.Sequential(
+            nn.Linear(frequency_embedding_size, hidden_size, bias=True),
+            nn.SiLU(),
+            nn.Linear(hidden_size, hidden_size, bias=True),
+        )
+        self.frequency_embedding_size = frequency_embedding_size
+
+    @staticmethod
+    def timestep_embedding(t, dim, max_period=10000):
+        """
+        Create sinusoidal timestep embeddings.
+        :param t: a 1-D Tensor of N indices, one per batch element.
+                          These may be fractional.
+        :param dim: the dimension of the output.
+        :param max_period: controls the minimum frequency of the embeddings.
+        :return: an (N, D) Tensor of positional embeddings.
+        """
+        # https://github.com/openai/glide-text2im/blob/main/glide_text2im/nn.py
+        half = dim // 2
+        freqs = torch.exp(-math.log(max_period) * torch.arange(start=0, end=half, dtype=torch.float32) / half)
+        freqs = freqs.to(device=t.device)
+        args = t[:, None].float() * freqs[None]
+        embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
+        if dim % 2:
+            embedding = torch.cat([embedding, torch.zeros_like(embedding[:, :1])], dim=-1)
+        return embedding
+
+    def forward(self, t, dtype):
+        t_freq = self.timestep_embedding(t, self.frequency_embedding_size)
+        if t_freq.dtype != dtype:
+            t_freq = t_freq.to(dtype)
+        t_emb = self.mlp(t_freq)
+        return t_emb
+
 
 class FinalLayer(nn.Module):
     """
