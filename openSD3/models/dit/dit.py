4da3f69d6453b8bc929d60c1e454d12f9d0ce396
BrunoFANG1 <1324441676@qq.com>
2024-03-31 20:31:43 +0000

    Update DiT and y_embedding

--- openSD3/models/dit/dit.py
+++ openSD3/models/dit/dit.py
@@ -1,7 +1,16 @@
 import torch
 import torch.nn as nn
 import einops
-from openSD3.models.layers.blocks import modulate, RMSNorm, MultiHeadAttention
+import clip
+import numpy as np
+from openSD3.models.layers.blocks import modulate, RMSNorm, MultiHeadAttention, FinalLayer
+
+from transformers import (
+    CLIPProcessor, 
+    CLIPTextModel,
+    T5Tokenizer, 
+    T5EncoderModel,
+)
 
 class MMDiTBlock(nn.Module):
     """
@@ -70,15 +79,111 @@ class MMDiTBlock(nn.Module):
         return c, x
 
 
+class DiT(nn.Module):
+    """
+    Diffusion Transformer
+    """
+    def __init__(
+            self,
+            input_size = (16, 32, 32),
+            in_channels = 4,
+            patch_size=(1, 2, 2),
+            emb_size=768,
+            depth=2,
+            num_heads=8,
+            mlp_ratio=4,
+            class_dropout_prob=0.1,
+            learn_sigma=True,
+            condition="text",
+            no_temporal_pos_emb=False,
+            caption_channels=512,
+            model_max_length=77,
+            dtype=torch.float32,
+            enable_flashattn=False,
+            enable_layernorm_kernel=False,
+            enable_sequence_parallelism=False
+    ):
+        super().__init__()
+        self.learn_sigma = learn_sigma
+        self.in_channels = in_channels   ##### Why input chanel is 4 instead of 3 (RGB)
+        self.out_channels = in_channels * 2 if learn_sigma else in_channels
+        self.emb_size = emb_size
+        self.patch_size = patch_size
+        self.input_size = input_size
+        num_patches = np.prod([input_size[i] // patch_size[i] for i in range(3)])
+        self.num_patches = num_patches
+        self.num_temporal = input_size[0] // patch_size[0]
+        self.num_spatial = num_patches // self.num_temporal
+        self.num_heads = num_heads
+        self.dtype = dtype
+        self.text_tokenizer_1 = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+        self.text_model_1 = CLIPTextModel.from_pretrained("openai/clip-vit-base-patch32")
+        self.text_tokenizer_2 = CLIPProcessor.from_pretrained("openai/clip-vit-large-patch14")
+        self.text_model_2 = CLIPTextModel.from_pretrained("openai/clip-vit-large-patch14")
+        self.text_tokenizer_3 = T5Tokenizer.from_pretrained("google/flan-t5-large")
+        self.text_model_3 = T5EncoderModel.from_pretrained("google/flan-t5-large")
+        self.blocks = nn.ModuleList(
+            [
+                MMDiTBlock(
+                    emb_size,
+                    num_heads,
+                    mlp_ratio=mlp_ratio,
+                    enable_RMS_Norm =False
+                )
+                for _ in range(depth)
+            ]
+        )
+        self.final_layer = FinalLayer(emb_size, np.prod(self.patch_size), self.out_channels) ###???
+    
+    def unpatchify(self, x):
+        c = self.out_channels
+        t, h, w = [self.input_size[i] // self.patch_size[i] for i in range(3)]
+        pt, ph, pw = self.patch_size
+
+        x = x.reshape(shape=(x.shape[0], t, h, w, pt, ph, pw, c))
+        x = einops.rearrange(x, "n t h w r p q c -> n c t r h p w q")
+        imgs = x.reshape(shape=(x.shape[0], c, t * pt, h * ph, w * pw))
+        return imgs
+
+    def y_embedding(self, Caption):
+        return y
+    
+    def c_embedding(self, Caption):
+        cap_emb_1 = self.text_tokenizer_1(text=Caption, return_tensors="pt", padding='max_length', max_length=77, truncation=True)
+        cap_emb_1 = self.text_model_1(**cap_emb_1)
+        text_emb_1 = cap_emb_1.last_hidden_state
+        # print(cap_emb_1.last_hidden_state.shape) # [2, 77, 512]
+
+        cap_emb_2 = self.text_tokenizer_2(text=Caption, return_tensors="pt", padding='max_length', max_length=77, truncation=True)
+        cap_emb_2 = self.text_model_1(**cap_emb_2)
+        text_emb_2 = cap_emb_2.last_hidden_state
+        # print(cap_emb_2.last_hidden_state.shape) # [2, 77, 512]
+
+        cap_emb_3 = self.text_tokenizer_3(text=Caption, return_tensors="pt", padding='max_length', max_length=77, truncation=True) 
+        cap_emb_3 = self.text_model_3(input_ids=cap_emb_3['input_ids'], attention_mask=cap_emb_3['attention_mask'])       
+        # print(cap_emb_3.last_hidden_state.shape) # [2, 77, 1024]
+        text_emb_3 = cap_emb_3.last_hidden_state
+
+        assert  text_emb_1.shape[2] + text_emb_2.shape[2] == text_emb_3.shape[2] , 'You need to make sure their embedding size is the same (Due to memery issue, I did not follow the same T5-xxl model in the orignal paper)'
+        output_embedding = torch.cat([torch.cat([text_emb_1, text_emb_2], dim=2), text_emb_3], dim=1)
+
+        return output_embedding
+
+    def forward(self, x, t, Caption):
+        return None
+
 def test():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     xx = torch.rand(10,25,768).to(device)
     cc = torch.rand(10,28,768).to(device)
     yy = torch.rand(10,768).to(device)
-    pack = MMDiTBlock(768, 8).to(device)
-    output_c, output_x = pack(yy, cc, xx)
-    print(output_c.shape)
-    print(output_x.shape)
+    Caption = ["A photo of a cat", "A photo of a dog"]
+    
+    pack = DiT()
+    output = pack.c_embedding(Caption)
+    print(output.shape)
+    # print(output_x.shape)
+    return None
 
 if __name__ == "__main__":
     test()
--- openSD3/models/layers/blocks.py
+++ openSD3/models/layers/blocks.py
@@ -24,6 +24,23 @@ class RMSNorm(nn.Module):
         x = x/(RMS + self.epsilon) * self.g
         return x
 
+class FinalLayer(nn.Module):
+    """
+    The final layer of DiT.
+    """
+
+    def __init__(self, hidden_size, num_patch, out_channels):
+        super().__init__()
+        self.norm_final = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
+        self.linear = nn.Linear(hidden_size, num_patch * out_channels, bias=True)
+        self.adaLN_modulation = nn.Sequential(nn.SiLU(), nn.Linear(hidden_size, 2 * hidden_size, bias=True))
+
+    def forward(self, x, c):
+        shift, scale = self.adaLN_modulation(c).chunk(2, dim=1)
+        x = modulate(self.norm_final, x, shift, scale)
+        x = self.linear(x)
+        return x
+
 class MultiHeadAttention(nn.Module): #### Consider add FlashAttention
     def __init__(
             self,
