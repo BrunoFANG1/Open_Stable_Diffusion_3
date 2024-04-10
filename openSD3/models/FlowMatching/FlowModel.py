147d472650cda19cf9145062766c469e818e99ec
BrunoFANG1 <1324441676@qq.com>
2024-04-10 04:03:21 +0000

    Finish Flow Matching; Wait for further testing

--- openSD3/models/FlowMatching/FlowModel.py
+++ openSD3/models/FlowMatching/FlowModel.py
@@ -1,21 +1,23 @@
 import torch
 import torch.nn as nn
+from openSD3.models.dit.dit import DiT
+from openSD3.schedulers.time_scheduler import LogitNormalSampler
 
-class FlowBasedModel(nn.Module):
+class FlowMatching(nn.Module):
     def __init__(self,
                 model,
-                loss,
-                batch_size
-                
+                loss               
     ):
         super().__init__()
         self.flow_prediction_model = model
         self.loss = loss
-        self.batch_size = batch_size
     
-    def train(self, x_0, epsilon, t, Caption):
-
-        z_t = (1-t) * x_0 + t * epsilon
+    def forward(self, x_0, epsilon, t, Caption):
+        device = x_0.device
+        time = torch.tensor(t).to(device)
+        time = time.unsqueeze(-1).unsqueeze(-1).unsqueeze(-1).unsqueeze(-1) 
+        
+        z_t = (1-time) * x_0 + time * epsilon
         u_t = -x_0 + epsilon
         v_t = self.flow_prediction_model(z_t, t, Caption)
         loss_ = self.loss(u_t, v_t)
@@ -29,3 +31,25 @@ class FlowBasedModel(nn.Module):
             z_t += step * v_t
 
         return z_t
+
+def test():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    xx = torch.rand(2,4, 16,32,32).to(device)
+    epsilon = torch.rand_like(xx).to(device)
+    Caption = ["A photo of a cat", "A photo of a dog"]
+
+    t = [2,7]
+    t = torch.tensor(t)
+    loss_func = nn.MSELoss()
+    model = DiT(learn_sigma=False)
+
+    match = FlowMatching(model=model, loss=loss_func)
+    output = match(xx, epsilon, t, Caption)
+
+    print(output)
+
+    return None
+
+if __name__ == "__main__":
+
+    test()
